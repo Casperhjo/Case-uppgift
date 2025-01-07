58cc087b3cd593bf9576b57611839c3e1936ba97
Casperhjo <casper.hjorth4@gmail.com>
2025-01-07 23:21:05 +0100

    Update main.bash

--- main.bash
+++ main.bash
@@ -586,8 +586,8 @@ folder_View() {
         group=$(ls -ld "$folder_path" | awk '{print $4}')
         size=$(du -sh "$folder_path" | awk '{print $1}')
         last_modified=$(stat -c '%y' "$folder_path")
-        sticky_bit=$(echo "$permissions" | grep -q 't' && echo "On" || echo "Off")
-        setgid=$(echo "$permissions" | grep -q 's' && echo "On" || echo "Off")
+        sticky_bit=$(stat -c '%A' "$folder_path" | grep -q 't' && echo "On" || echo "Off")
+        setgid=$(stat -c '%A' "$folder_path" | grep -q 's' && echo "On" || echo "Off")
 
         # Visa attribut
         echo "Owner:           $owner"
